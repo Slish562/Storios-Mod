e1e2cc23bb32d62d5e71081d73f9ae85efe0d757
Slish <90574933+Slish562@users.noreply.github.com>
2024-06-26 14:02:49 +0300

    fixes

---
+++ src/main/java/com/example/examplemod/block/DirectionalBlock.java
@@ -0,0 +1,32 @@
+package com.example.examplemod.block;
+
+import net.minecraft.client.resources.model.Material;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+
+public class DirectionalBlock extends Block {
+    public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.values());
+
+    public DirectionalBlock() {
+        super(BlockBehaviour.Properties.of().strength(2.5F).sound(SoundType.WOOD).noOcclusion());
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
+
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(new Property[]{FACING});
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getNearestLookingDirection().getOpposite());
+    }
+}
