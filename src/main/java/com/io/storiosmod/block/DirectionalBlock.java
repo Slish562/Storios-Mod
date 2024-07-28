f946a00f820d9e538a5bd21c0fc1150e1751a9dd
Slish <90574933+Slish562@users.noreply.github.com>
2024-07-28 19:32:58 +0300

    hitbox fix 1

--- src/main/java/com/io/storiosmod/block/DirectionalBlock.java
+++ src/main/java/com/io/storiosmod/block/DirectionalBlock.java
@@ -3,6 +3,7 @@ package com.io.storiosmod.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.context.BlockPlaceContext;
@@ -30,9 +31,12 @@ public class DirectionalBlock extends Block implements SimpleWaterloggedBlock {
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
     public static final DirectionProperty FACING = BlockStateProperties.FACING;
 
-    private static final VoxelShape SHAPE_UD = Block.box(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D);
-    private static final VoxelShape SHAPE_NS = Block.box(1.0D, 1.0D, 0.0D, 15.0D, 15.0D, 16.0D);
-    private static final VoxelShape SHAPE_EW = Block.box(0.0D, 1.0D, 1.0D, 16.0D, 15.0D, 15.0D);
+    private static final VoxelShape SHAPE_UP = Block.box(2.0D, 0.0D, 2.0D, 10.0D, 10.0D, 10.0D);
+    private static final VoxelShape SHAPE_DOWN = Block.box(2.0D, 2.0D, 2.0D, 10.0D, 16.0D, 10.0D);
+    private static final VoxelShape SHAPE_EAST = Block.box(0.0D, 2.0D, 2.0D, 10.0D, 10.0D, 10.0D);
+    private static final VoxelShape SHAPE_WEST = Block.box(2.0D, 2.0D, 2.0D, 16.0D, 10.0D, 10.0D);
+    private static final VoxelShape SHAPE_NORTH = Block.box(2.0D, 2.0D, 2.0D, 10.0D, 10.0D, 16.0D);
+    private static final VoxelShape SHAPE_SOUTH = Block.box(2.0D, 2.0D, 0.0D, 10.0D, 10.0D, 10.0D);
 
     public DirectionalBlock() {
         super(BlockBehaviour.Properties.of().mapColor(DyeColor.WHITE).requiresCorrectToolForDrops().strength(2F, 6.0F).noOcclusion().dynamicShape().lightLevel((i) -> 2).emissiveRendering((state, level, pos) -> true));
@@ -54,14 +58,22 @@ public class DirectionalBlock extends Block implements SimpleWaterloggedBlock {
     }
 
     public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        switch (state.getValue(FACING).getAxis()) {
-            case X:
-                return SHAPE_EW;
-            case Z:
-                return SHAPE_NS;
-            default:
-                return SHAPE_UD;
+        switch (state.getValue(FACING)) {
+            case UP:
+                return SHAPE_UP;
+            case DOWN:
+                return SHAPE_DOWN;
+            case EAST:
+                return SHAPE_EAST;
+            case WEST:
+                return SHAPE_WEST;
+            case NORTH:
+                return SHAPE_NORTH;
+            case SOUTH:
+                return SHAPE_SOUTH;
         }
+        return SHAPE_UP;
+
     }
 
     @Nullable
